665f843a73f06c2c84b0af0e8fb40621e4139d3b
Damian Betebenner <dbetebenner@nciea.org>
2025-05-22 05:53:49 -0400

    Tidying up sgpFlow calculation script

--- sgpFlow/Demonstration_sgpFlow_2024_2025.R
+++ sgpFlow/Demonstration_sgpFlow_2024_2025.R
@@ -1,17 +1,18 @@
 ###################################################################
-### DEMONSTRATION sgpFlow analysis for 2024
+### DEMONSTRATION sgpFlow analysis for 2025
 ###################################################################
 
 ### Load packages
 require(sgpFlow)
 require(sgpFlowMatrices)
 #debug(sgpFlow::sgpFlow)
+debug(sgpFlow::getPercentileTrajectories)
 
 ### Load data
 load("Data/Demonstration_Data_LONG_2024_2025.Rdata")
 
 # Use super-cohort matrices
-projection.splineMatrices <- sgpFlowMatrices[['DEMO_sgpFlowMatrices']][['2023_2024']][['SUPER_COHORT']]
+projection.splineMatrices <- sgpFlowMatrices[['DEMO_sgpFlowMatrices']][['2024_2025']][['SUPER_COHORT']]
 
 # Source and create sgpFlow configurations for MATHEMATICS and READING
 source("SGP_CONFIG/sgpFlow/2024_2025/MATHEMATICS.R")
@@ -22,4 +23,5 @@ DEMO_sgpFlow.config <- c(sgpFlow_MATHEMATICS.config, sgpFlow_READING.config)
 Demonstration_sgpFlow <- sgpFlow(
                     sgp_object = Demonstration_Data_LONG_2024_2025,
                     sgpFlow.config = DEMO_sgpFlow.config,
-                    projection.splineMatrices = projection.splineMatrices)
+                    projection.splineMatrices = projection.splineMatrices,
+                    parallel.config = NULL)
